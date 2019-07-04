18932474b1b5c4dd93c4958737735b1822c57347
Richard Pitts <richardepitts@gmail.com>
2019-07-04 01:23:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,3 @@ const app = "I don't do much."
   }
 }
 
- document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-  if (e.which === 39) {
-    moveDodgerRight()
-  }
-}
